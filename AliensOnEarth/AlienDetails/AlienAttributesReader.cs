3b87ce8568236dc35153b7059b88daa403c34b7b
kasperavi <aviatwebz@gmail.com>
2015-07-20 22:00:12 +0530

    Changed Xml path

--- AliensOnEarth/AlienDetails/AlienAttributesReader.cs
+++ AliensOnEarth/AlienDetails/AlienAttributesReader.cs
@@ -27,7 +27,7 @@ namespace AliensOnEarth.AlienDetails
             try
             {
                 //Path to the Xml file
-                XmlReader xmlReader = XmlReader.Create("C:/Users/Kasper/Documents/Visual Studio 2012/Projects/optimized/AliensOnEarth/AliensOnEarth/AlienAttributesList.xml");
+                XmlReader xmlReader = XmlReader.Create(@"C:/Users/Kasper/Documents/Visual Studio 2012/Projects/optimized/AliensOnEarth/AliensOnEarth-improved-/AliensOnEarth/AlienAttributesList.xml");
                 while (xmlReader.Read())
                 {
                     if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "alien") && (xmlReader.HasAttributes))
